9291408ec84592acd5fbbda102c2c5d4675a3651
Ryan Liu <ryan@ryanliu6.xyz>
2024-06-10 20:35:22 -0700

    Uppercasing global variables

--- generate_compose.py
+++ generate_compose.py
@@ -10,13 +10,13 @@ from vigor.compose import Compose
 from vigor.utils import get_immediate_subdirectories
 
 # Some global stuff I don't know how to not abstract
-root_dir = os.path.dirname(os.path.abspath(__name__))
-all_services = get_immediate_subdirectories(full_path=root_dir, ignore=[".git"])
-core_services = ["traefik"]
+ROOT_PATH = os.path.dirname(os.path.abspath(__name__))
+ALL_SERVICES = get_immediate_subdirectories(full_path=ROOT_PATH, ignore=[".git"])
+CORE_SERVICES = ["traefik"]
 
 
 @click.command()
-@click.argument("services", type=click.Choice(all_services), nargs=-1)
+@click.argument("services", type=click.Choice(ALL_SERVICES), nargs=-1)
 @click.option("-v", "--verbose", is_flag=True, default=False, help="Verbose mode.")
 @click.option("-a", "--all", is_flag=True, default=False, help="Generate Compose file for all possible services.")
 def generate_config(services, verbose, all):
@@ -27,19 +27,19 @@ def generate_config(services, verbose, all):
     logging.basicConfig(level=log_level)
 
     if all:
-        services = all_services
+        services = ALL_SERVICES
     else:
         # Is this more performant than iterating through core_services and doing `in` check with append?
-        services = list(set(services + core_services))
+        services = list(set(services + CORE_SERVICES))
 
     logging.info(f"Services selected: {services}")
     files = []
     for service in services:
-        files.append(os.path.join(root_dir, service, "docker-compose.yml"))
+        files.append(os.path.join(ROOT_PATH, service, "docker-compose.yml"))
 
     aggregated_env = aggregate_env_file(services=services)
 
-    aggregated_compose = os.path.join(root_dir, "docker-compose.yml")
+    aggregated_compose = os.path.join(ROOT_PATH, "docker-compose.yml")
     with open(aggregated_compose, "w") as compose_file:
         compose = Compose("Focus")
         compose_config = compose.generate_compose_file(files=files, env=aggregated_env)
@@ -59,7 +59,7 @@ def aggregate_env_file(services: List) -> str:
     """
     env_files = []
     for service in services:
-        absolute_path = os.path.join(root_dir, service)
+        absolute_path = os.path.join(ROOT_PATH, service)
         for item in os.scandir(absolute_path):
             if item.is_file and item.name.endswith(".env"):
                 env_files.append(item.path)
@@ -76,7 +76,7 @@ def aggregate_env_file(services: List) -> str:
                 split = line.split("=")
                 aggregate_config[split[0]] = split[1]
 
-    aggregate_filename = os.path.join(root_dir, ".env")
+    aggregate_filename = os.path.join(ROOT_PATH, ".env")
     with open(aggregate_filename, "w") as write_file:
         for key, value in aggregate_config.items():
             write_file.write(f"{key}={value}")
